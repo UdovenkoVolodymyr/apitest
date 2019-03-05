12942be4a5158a96e0de5f94c49a3d7b98dbb200
UdovenkoVolodymyr <uvv333@hotmail.com>
2019-03-05 19:33:32 +0200

    client file

--- api_client.py
+++ api_client.py
@@ -2,7 +2,10 @@ import requests
 
 #url = "https://apinlp-pro.herokuapp.com/messages"
 url = "http://127.0.0.1:5000/messages"
-req_str = "test_test_TEST"
-response = requests.post(url, data=req_str, headers={"Content-Type": "text/plain"})
-#response = requests.get(url)
+input_str = "example str TEST"
+
+headers = {'Content-type': 'text/plain'}
+response = requests.request(method="GET", url=url, data=input_str, headers=headers)
+#response = requests.post(url, data=req_str, headers=headers)
+
 print(response.text)
